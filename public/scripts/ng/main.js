f0c9df38d37a2fcc272c571438e4183ecc7be341
martin <martin@rga.com>
2015-06-14 14:29:17 -0300

    plug in images

--- public/scripts/ng/main.js
+++ public/scripts/ng/main.js
@@ -24,8 +24,8 @@ myApp.controller('HomeController', function($scope,$rootScope,$location, $http){
           console.log(data);
           for (var i = 0; i < data.length; i++) {
             var d = data[i]; 
-            d.assets = {}
-            d.assets.image = "http://www.todobuenosaires.com/images/lugares/recoleta/gr/florsolar02.jpg";
+            
+            
             
           };
         $scope.recorridos = data;
